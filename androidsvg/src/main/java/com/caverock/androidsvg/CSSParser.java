2a3b274c8d1b88206d034c26e5ea6e298a4e97fe
ewaldc <ewald_comhaire@hotmail.com>
2020-05-27 21:30:30 +0200

    Update CSSParser.java

--- androidsvg/src/main/java/com/caverock/androidsvg/CSSParser.java
+++ androidsvg/src/main/java/com/caverock/androidsvg/CSSParser.java
@@ -1166,7 +1166,7 @@ class CSSParser
          if (!scan.empty() && !scan.consume(';'))
             throw new CSSParseException("Invalid @media rule: expected '}' at end of rule set");
 
-         if (SVG.getFileResolver() != null && (!mediaList || mediaMatches(mediaList, deviceMediaType))) {
+         if (SVG.getFileResolver() != null && (mediaList == null || mediaMatches(mediaList, deviceMediaType))) {
             String  css = SVG.getFileResolver().resolveCSSStyleSheet(file);
             if (css == null)
                return;
